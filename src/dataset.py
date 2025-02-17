1a54e615cefac4739638600a4166ec85f3dddde5
DatateamPlus <support@datateamplus.com>
2025-02-16 19:26:38 -0500

    Add sum_of_two_numbers function and create initial notebook

---
+++ src/dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+ 
+data = {
+    'name': ['John', 'Anna', 'Peter', 'Linda'],
+    'location': ['New York', 'Paris', 'Berlin', 'London'],
+    'age': [24, 13, 53, 33]
+    
+}
+
+df = pd.DataFrame(data)
+
+
+df.head(2)
+
+df.replace('New York', 'New York City')
